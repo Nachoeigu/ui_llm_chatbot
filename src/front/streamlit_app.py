0fd9c9a5e218b518ca35ae02408bd4986b0e6d1d
Nacho Eigu <ignacio.eiguren@gmail.com>
2024-07-05 12:13:43 +0200

    Update streamlit_app.py

--- src/front/streamlit_app.py
+++ src/front/streamlit_app.py
@@ -20,35 +20,36 @@ import streamlit as st
 logger = logging.getLogger(__name__)
 if os.getenv("LANGCHAIN_DEBUG_LOGGING") == 'True':
     set_debug(True)
-#model = ChatVertexAI(model="gemini-pro", temperature=0)
-#model = ChatGoogleGenerativeAI(model = 'gemini-1.5-pro', temperature = 0)
-#model = ChatOpenAI(model = 'gpt-4o', temperature = 0)
-model = ChatOpenAI(model = 'gpt-3.5-turbo', temperature = 0)
-memory = ConversationTokenBufferMemory(
-    llm=model, 
-    max_token_limit=64000
-)
-
-llm_chat = Chatbot(
-    model = model,
-    system_prompt = BASIC_PROMPT #CUSTOM_PROMPTS['Python-Engineer']
-)
-logger.info('starting')
 
 if __name__ == '__main__':
-
+    if "llm_chat" not in st.session_state:
+        #st.session_state.model = ChatVertexAI(model="gemini-pro", temperature=0)
+        #st.session_state.model = ChatGoogleGenerativeAI(model = 'gemini-1.5-pro', temperature = 0)
+        #st.session_state.model = ChatOpenAI(model = 'gpt-4o', temperature = 0)
+        st.session_sate.model = ChatOpenAI(model = 'gpt-3.5-turbo', temperature = 0)
+        st.session_sate.llm_chat = Chatbot(
+            model = st.session_sate.model,
+            system_prompt = BASIC_PROMPT #CUSTOM_PROMPTS['Python-Engineer']
+        )
+    if "memory" not in st.session_state:
+        st.session_sate.memory = ConversationTokenBufferMemory(
+            llm=st.session_sate.model, 
+            max_token_limit=64000
+        )
 
     st.title("🦜🔗 Chat with me!")
 
     with st.form("my_form"):
-        logger.info(memory.chat_memory.messages)
+        logger.info(st.session_sate.memory.chat_memory.messages)
         user_query = st.text_area("Enter your question:", "I would like to know more about...")
         submitted = st.form_submit_button("Submit")
         if submitted:
-            output = llm_chat(user_query=user_query,memory=memory)
-            memory.save_context(
+            output = st.session_sate.llm_chat(
+                user_query=user_query,
+                memory=st.session_sate.memory
+            )
+            st.session_sate.memory.save_context(
                 {'Past User Message': user_query},
                 {'Past AI Message': output}
             )
             st.info(output)
-
