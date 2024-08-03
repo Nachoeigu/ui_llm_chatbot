fc172887187a09b00354461f5a2719dba191a814
ignacio.eiguren@gmail.com <ignacio.eiguren@gmail.com>
2024-08-03 13:12:00 +0200

    .

--- src/front/streamlit_app.py
+++ src/front/streamlit_app.py
@@ -21,19 +21,28 @@ if os.getenv("LANGCHAIN_DEBUG_LOGGING") == 'True':
     set_debug(True)
 
 if __name__ == '__main__':
+    st.set_page_config(
+        initial_sidebar_state="expanded",
+        page_title="AI Chatbot",
+        page_icon="⚡",
+        layout="wide"
+    )
     if "prompt" not in st.session_state:
         st.session_state.prompt = 'Default-LLM'
+        
     if "llm_chat" not in st.session_state:
         st.session_state = model_selection(st.session_state, 'OpenAI: gpt-4o-mini', 0.5, st.session_state.prompt)
     if "memory" not in st.session_state:
         st.session_state.memory = ConversationTokenBufferMemory(llm=st.session_state.model)
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-    if "user_query" not in st.session_state:
-        st.session_state.user_query = ""
+    if "input_question" not in st.session_state:
+        st.session_state.input_question = ''
 
-    selected_prompt = st.selectbox("Choose system prompt", CUSTOM_PROMPTS, index=0)
-    selected_model = st.selectbox("Choose model", AVAILABLE_MODELS, index=4)
-    temperature = st.slider("Adjust Temperature", min_value=0.0, max_value=1.0, value=0.5, step=0.1)
+    selected_prompt = st.selectbox("**CHOOSE SYSTEM PROMPT**", CUSTOM_PROMPTS, index=0)
+    selected_model = st.selectbox("**CHOOSE MODEL**", AVAILABLE_MODELS, index=4)
+    temperature = st.slider("**SET TEMPERATURE**", min_value=0.0, max_value=1.0, value=0.5, step=0.1)
     if st.button("Clean Memory", type = 'primary'):
         st.session_state.memory = ConversationTokenBufferMemory(llm=st.session_state.model)
 
@@ -41,32 +50,40 @@ if __name__ == '__main__':
     if (selected_model != st.session_state.model_name)|(st.session_state.temperature != temperature)|(st.session_state.prompt != selected_prompt):
         st.session_state = model_selection(st.session_state, selected_model, temperature, selected_prompt)
 
-    st.title("🦜🔗 Chat with me!")
     saving_memory_in_file(st.session_state.memory.chat_memory.messages)
     logger.info(f"Model settings:\n- Prompt:{st.session_state.llm_chat.system_prompt}\n- Model name:{st.session_state.llm_chat.model}\n- Temperature:{st.session_state.temperature}")
+    with st.chat_message("system"):
+        if st.session_state.prompt == 'Default-LLM':
+            pass
+        else:
+            st.markdown(CUSTOM_PROMPTS[st.session_state.prompt])
+
     for message in st.session_state.memory.chat_memory.messages:
         if isinstance(message, HumanMessage):
-            st.markdown(format_message(message.content, True), unsafe_allow_html=True)
-        if isinstance(message, AIMessage):
-            st.markdown(format_message(message.content, False), unsafe_allow_html=True)
-    
-    typing_placeholder = st.empty()
-    with st.form(key='submission'):
-        user_query = st.text_area("Put your question:", st.session_state.user_query)
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            typing_placeholder.markdown(f"**Generating response...**")
-        
+            role = "user"
+        elif isinstance(message, AIMessage):
+            role = "assistant"
+        with st.chat_message(role):
+            st.markdown(message.content)    
+    if user_query := st.chat_input("Put your messages here..."):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(user_query)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": user_query})
+
+        response = f"Echo: {user_query}"
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
             output = st.session_state.llm_chat(
                 user_query=user_query,
                 memory=st.session_state.memory
             )
-            typing_placeholder.empty()
-            st.session_state.memory.save_context(
+            st.markdown(output)
+        
+        st.session_state.memory.save_context(
                 {'input': user_query},
                 {'output': output}
             )
-            st.session_state.user_query = " "
-            st.info(output)
-            st.rerun()
-        
+            
+        st.rerun()
+
